d6f5b9579e74a914d95e80ba22bca9aaf7e5e692
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-04-02 15:32:02 -0400

    Added render/NavLink

--- client/src/App.js
+++ client/src/App.js
@@ -22,8 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <Route exact path="/" render={()=> <MovieList />} />
+        <Route path="/movies/:id" render={(props)=> <Movie {...props} />} />
       </div>
     );
   }
